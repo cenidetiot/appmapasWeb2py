ba37e986eb8c671cfb3801aa94af7ef8de6e0f59
haidee09 <haideeram97@gmail.com>
2018-02-09 16:24:41 -0600

    first commit

--- static/js/appMapas.js
+++ static/js/appMapas.js
@@ -1,8 +1,9 @@
-var map = L.map("mapid").setView([18.876551, -99.220100], 18);
+var map = L.map("mapid").setView([18.876551, -99.220100], 19);
 L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 var coordinatesConverted = []; 
+var polylineArrayCoordinates = [];
 var editableLayers = new L.FeatureGroup();
 map.addLayer(editableLayers);
 
@@ -21,6 +22,12 @@ var drawControl = new L.Control.Draw({
             },        
             showArea: true
         },
+        polyline: {
+            shapeOptions: {
+                color: '#008B8B',
+                weight: 8
+            }
+        },
         circle: false,
         circlemarker: false
     },
@@ -37,6 +44,7 @@ L.marker([18.876551, -99.220100]).addTo(map)
 
 map.on('draw:created', function (e) {
    var type = e.layerType;
+   console.log(type);
    var layer = e.layer;
    if (type === 'marker'){
         console.log("CREANDO MARCADOR");
@@ -48,7 +56,7 @@ map.on('draw:created', function (e) {
         var polygon = layer.toGeoJSON();
         var polygonCoordinates = polygon['geometry']['coordinates'];
 
-        //CONVERT COORDINATES [LON,LAT] GeoJSON [IN LAT],LON COORDINATES.
+        //CONVERT COORDINATES [LON,LAT] GeoJSON IN [LAT,LON] COORDINATES.
         for(let i=0; i<polygonCoordinates.length;i++){
           for(let j=0; j<polygonCoordinates[i].length;j++){
             coordinatesConverted.push([polygonCoordinates[i][j][1],polygonCoordinates[i][j][0]]);         
@@ -56,7 +64,21 @@ map.on('draw:created', function (e) {
         }
         console.log(JSON.stringify(polygon));
         console.log(polygonCoordinates);
+        console.log("Coordenadas  [lat,long] del polígono ");
         console.log(coordinatesConverted);   
+    }
+    if(type === 'polyline'){
+        console.log("CREANDO POLILÍNEA");
+        var polylineCoordinates = layer.getLatLngs();
+        //CONVERT POLYLINE COORDINATES INTO ARRAY OF COORDINATES 
+        polylineCoordinates.forEach(element => {
+            polylineArrayCoordinates.push([element['lat'],element['lng']])
+        });
+        console.log(polylineCoordinates);
+        console.log(polylineArrayCoordinates);
+    }
+    if(type === 'rectangle'){
+        console.log("CREANDO RECTANGULO");
     }
    // Do whatever else you need to. (save to db; add to map etc)
    editableLayers.addLayer(layer);
